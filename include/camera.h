46ad7a010b5078b8f9deac0fc622faa2d12e11ec
IanosAlex-Marian <ianosalternative@gmail.com>
2026-04-27 21:19:05 +0300

    Made camera work with keyboard and mouse

---
+++ include/camera.h
@@ -0,0 +1,38 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <glm/glm.hpp>
+
+enum Camera_Movement {
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT
+};
+
+class Camera {
+public:
+    glm::vec3 Position;
+    glm::vec3 Front;
+    glm::vec3 Up;
+    glm::vec3 Right;
+    glm::vec3 WorldUp;
+
+    float Yaw;
+    float Pitch;
+
+    float Speed;
+    float Sensitivity;
+
+    Camera(glm::vec3 position);
+
+    glm::mat4 GetViewMatrix();
+
+    void ProcessKeyboard(Camera_Movement direction, float deltaTime);
+    void ProcessMouse(float xoffset, float yoffset);
+
+private:
+    void updateVectors();
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,50 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <iostream>
-#include <direct.h>
 #include "shaderClass.h"
 #include "VAO.h"
 #include "VBO.h"
 #include "EBO.h"
+#include "camera.h"
+
+/* Initialize window width and height */
+const unsigned int WIDTH = 800, HEIGHT = 600;
+
+/* Define camera settings */
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+
+/* Mouse callback */
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    static float lastX = WIDTH, lastY = HEIGHT;
+    static bool firstMouse = true;
+
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouse(xoffset, yoffset);
+}
 
 /* Viewport changer */
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 /* Input function */
-void processInput(GLFWwindow* window);
+void processKeyboard(GLFWwindow* window);
+
+/* Define time variables */
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
 
 int main() {
 
@@ -70,9 +103,6 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    /* Initialize window width and height */
-    const unsigned int WIDTH = 800, HEIGHT = 600;
-
     /* Initialize window */
     GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Solar System", NULL, NULL);
     if (!window) {
@@ -83,6 +113,10 @@ int main() {
 
     /* Make window be current context */
     glfwMakeContextCurrent(window);
+
+    /* Initialize mouse callback */
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     /* Initialize glad */
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
@@ -122,14 +156,18 @@ int main() {
 
     /* Render loop */
     while (!glfwWindowShouldClose(window)) {
-        processInput(window);
+        processKeyboard(window);
+        
+        /* Update time variables */
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
 
         /* Use shader program */
         shaderProgram.Activate();
 
         /* Set time unfirom */
-        float time = glfwGetTime();
-        shaderProgram.setFloat("time", time);
+        shaderProgram.setFloat("time", currentFrame);
 
         /* Set Background */
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -138,23 +176,8 @@ int main() {
         /* Bind VAO */
         VAO1.Bind();
 
-        for (unsigned int i = 0; i < sizeof(cubePositions) / sizeof(glm::vec3); i++){
-            /* Create model matrix */
-            glm::mat4 model = glm::mat4(1.0f);
-            model= glm::translate(model, cubePositions[i]);
-
-            model = glm::rotate(model, time * (i + 1.0f)* 0.5f,
-                glm::vec3(1.0f, -1.0f, 0.0f));
-
-            /* Set model uniform */
-            shaderProgram.setMat4("model", model);
-
-            /* Draw */
-            glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
-        }
         /* Create view matrix */
-        glm::mat4 view = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        glm::mat4 view = camera.GetViewMatrix();
 
         /* Set view uniform */
         shaderProgram.setMat4("view", view);
@@ -166,6 +189,21 @@ int main() {
 
         /* Set projection uniform */
         shaderProgram.setMat4("projection", projection);
+
+        for (unsigned int i = 0; i < sizeof(cubePositions) / sizeof(glm::vec3); i++){
+            /* Create model matrix */
+            glm::mat4 model = glm::mat4(1.0f);
+            model= glm::translate(model, cubePositions[i]);
+
+            model = glm::rotate(model, currentFrame * (i + 1.0f)* 0.5f,
+                glm::vec3(1.0f, -1.0f, 0.0f));
+
+            /* Set model uniform */
+            shaderProgram.setMat4("model", model);
+
+            /* Draw */
+            glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+        }
 
         /* Swap Front & Back */
         glfwSwapBuffers(window);
@@ -187,9 +225,21 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
-/* Input function*/
-void processInput(GLFWwindow* window)
+/* Closing function */
+void processKeyboard(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
 }
